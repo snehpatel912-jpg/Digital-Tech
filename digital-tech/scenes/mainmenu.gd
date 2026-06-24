60d6e1ee4cb8132ae8ec4efcbe6472a9e4b9866c
snehpatel912-jpg <snehpatel912@gmail.com>
2026-06-24 12:39:39 +1200

    game levels
    added level selection and am almost done with the buttons

---
+++ digital-tech/scenes/mainmenu.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	# Change to your target scene using its file path
+	get_tree().change_scene_to_file("res://scenes/settings.tscn") 	
+
+
+func _on_button_2_pressed() -> void:
+		get_tree().change_scene_to_file("res://scenes/cars.tscn") 	
+
+
+func _on_button_3_pressed() -> void:
+		get_tree().change_scene_to_file("res://scenes/game.tscn") 	
---
+++ digital-tech/scenes/settingsbutton.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings.tscn") 
